c6203e69290803cbee1eff2d41d69f4469e9a69b
mernaghp <bigbenshuffleboard@gmail.com>
2024-06-15 16:37:52 -0400

    Scrape package dependencies from index.json

---
+++ pkg_scrape.py
@@ -0,0 +1,19 @@
+import json
+import os
+from pathlib import Path
+
+p = Path('C:/users/patri/anaconda3/pkgs')
+
+folders = [f for f in p.iterdir() if  f.is_dir()]
+pkgs_paths = [os.path.join(f, 'info', 'index.json')
+        for f in folders if os.path.isfile(os.path.join(f, 'info', 'index.json'))]
+
+data = {}
+for pkg_path in pkgs_paths:
+    p = Path(pkg_path)
+    pkg = p.parts[-3]
+    with open(pkg_path) as f:
+        data[pkg] = json.load(f)['depends']
+
+with open('Data/raw.json', 'w') as f:
+    json.dump(data, f)
