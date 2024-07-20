1c8b18753c621cea0dd98eb39f627c860205c710
mernaghp <bigbenshuffleboard@gmail.com>
2024-07-20 12:12:13 -0400

    Beefed-up analysis of degree distribution

--- dist.py
+++ dist.py
@@ -1,40 +1,89 @@
+"""
+Visualization and analysis of degree distribution
+"""
 # %%
+import json
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from matplotlib.ticker import PercentFormatter
 
 # %%
 A = np.load('Data/A.npy')
+with open('Data/clean.json') as f:
+    clean = json.load(f)
 
-def dist(axis: int, title: str) -> plt.Axes:
+# %%
+def summary_df(axis: int) -> pd.DataFrame:
     unique, counts = np.unique(A.sum(axis=axis), 
-                               return_counts=True)
+                               return_counts=True)    
     df = pd.DataFrame({'Vals': unique, 'Counts': counts})
     df['Percent'] = df['Counts'].div(df['Counts'].sum())
     df['Vals'] = df['Vals'].astype(int)
+    return df
+
+
+def dist(axis: int, xlabel: str) -> plt.Axes:
+    df = summary_df(axis)
 
     fig, ax = plt.subplots()
     df.plot.bar(x='Vals', y='Percent', ax=ax, 
-                legend=False, width=1, xlabel='x',
-                ylabel='P(x)')
-    ax.yaxis.set_major_formatter(PercentFormatter(xmax=1, decimals=0))
-    ax.set_title(title)
+                legend=False, width=1, 
+                ylabel='Fraction of nodes', 
+                edgecolor='k', linewidth=0.1)
+    ax.set_xlabel(xlabel)
     sns.despine()
     return ax
 
 
 # %%
-# From the perspective
-# that x number of other packages depend 
-# on any given package 
-# in-edges
-dist(1, 'Probability Distribution: # of in-edges');
+# In-degree: # of pkgs that depend on a given pkg
+dist(1, 'In-Degree');
 
 # %%
-# From the perspective
-# What is the probability that any given package
-# has x number of dependents
-# out-edges
-dist(0, 'Probability Distribution: # of out-edges');
+# Out-degree: # of pkgs that a given pkg depends on
+dist(0, 'Out-Degree');
+
+# %%
+# Bullet points for in-degree
+df = summary_df(axis=1)
+print(df[df['Vals'].between(0, 8)]['Percent'].sum()) # 0.9613
+
+# highest k-in
+# python: 378
+# vc: 175
+# vs2015_runtime: 174
+# numpy: 35
+# packaging: 33
+# zlib: 24
+
+# %%
+# Bullet points for out-degree
+df = summary_df(axis=0)
+print(df[df['Vals'].between(0, 11)]['Percent'].sum()) # 0.9511
+
+# %%
+# highest k-out
+[(k, len(clean[k])) for k in clean.keys() if len(clean[k]) >= 20]
+# [('_anaconda_depends', 62),
+#  ('anaconda-navigator', 21),
+#  ('arrow-cpp', 22),
+#  ('conda-build', 20),
+#  ('imagecodecs', 29),
+#  ('jupyter_server', 20),
+#  ('scrapy', 20),
+#  ('spyder', 41),
+#  ('streamlit', 24)]
+
+# %%
+[k for k in clean.keys() if len(clean[k]) == 0]
+# k-out == 0
+# ['blas',
+#  'ca-certificates',
+#  'icc_rt',
+#  'msys2-conda-epoch',
+#  'pybind11-abi',
+#  'tzdata',
+#  'vs2015_runtime',
+#  'winpty']
